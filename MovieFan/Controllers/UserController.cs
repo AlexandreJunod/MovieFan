09874989ba6d2e2d4d3b7ced320f1c3906daefb5
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2020-02-24 10:57:03 +0100

    initial commit

---
+++ MovieFan/Controllers/UserController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieFan.Models;
+
+namespace MovieFan.Controllers
+{
+    public class UserController : Controller
+    {
+        private List<User> getUsers()
+        {
+            return new List<User>()
+            {
+                new User(1, "Alexandre", "Junod", true),
+                new User(2, "Toto", "Titi", false),
+                new User(3, "Tata", "Tutu", false),
+            };
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            List<User> Users = getUsers();
+            return View(Users);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            User User = getUsers()[id - 1];
+            return View(User);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MovieFan/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFan.Models
+{
+    public class Movie
+    {
+        private int id;
+        private string title;
+        private string description;
+
+        public Movie(int id, string title, string description)
+        {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+        }
+
+        public int Id { get => this.id; }
+        public string Title { get => this.title; }
+        public string Description { get => this.description; }
+    }
+}
